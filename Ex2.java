87bb9694729f7472d4476b5683af734491bd9704
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-10-14 12:09:37 +0900

    Ex2.java修正

--- Ex2.java
+++ Ex2.java
@@ -20,6 +20,21 @@ class Ex2_0 extends Ex{
 	}
 }
 
+class Ex2_1 extends Ex{
+	void execute(){
+		int x;
+		int y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y) + "です。");
+		System.out.println("平均は" + (x + y) / 2 + "です。");
+	}
+}
+
 class Ex2_00 extends Ex{
 	void execute(){
 		Scanner stdIn = new Scanner(System.in);
